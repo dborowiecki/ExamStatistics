ca5630e7777d94a23158ca40b22bc86267cd0c32
dmn <dborowiecki7@gmail.com>
2019-05-24 13:57:00 +0200

    fix typo

--- DataAnalysis/analysis.py
+++ DataAnalysis/analysis.py
@@ -1,16 +1,16 @@
 import os
 import operator
+import warnings
 from collections import namedtuple, defaultdict
 from .csv_handle import CSVHandle, DatabaseCSVHandle
 
 
 class Analysis:
-    PROVINENCE_COUNT = 16
+    province_COUNT = 16
 
-    def __init__(self, file_name, csv_source = CSVHandle, encoding='windows-1250'):
+    def __init__(self, file_name, csv_source=CSVHandle, encoding='windows-1250'):
         self.csv = file_name
         self.csv_handler = csv_source(file_name, encoding=encoding)
-
 
         self.area_col = 'Terytorium'
         self.group_col = 'Przystąpiło/zdało'
@@ -18,41 +18,40 @@ class Analysis:
         self.gender_col = 'Płeć'
         self.population_col = 'Liczba osób'
 
-    def average_in_year(self, provinence, to_year,from_year=2010, gender=None):
-        years = list(range(from_year, to_year+1))
+    def average_in_year(self, province, to_year, from_year=2010, gender=None):
+        years = list(range(from_year, to_year + 1))
 
-        result = self.attendance_in_area(provinence, years, gender)
-        
-        #works for data sort ascending by years]
+        result = self.attendance_in_area(province, years, gender)
+
+        # works for data sort ascending by years]
         year_max = result[-1][self.year_col]
         average = 0
         for line in result:
             average = average + int(line[self.population_col])
             if line[self.year_col] > year_max:
-                year_max = line[self.year_col] 
+                year_max = line[self.year_col]
 
-        average = average / (int(year_max)+1-from_year)
+        average = average / (int(year_max) + 1 - from_year)
 
         return average
 
-    def percentage_of_pass(self, provinence, gender=None, years=None):
+    def percentage_of_pass(self, province, gender=None, years=None):
         params = {}
-        params[self.area_col] = provinence
+        params[self.area_col] = province.split(',')
 
         if gender is not None:
             params[self.gender_col] = gender
         if years is not None:
             params[self.year_col] = years
 
-        result          = self.get_data_by_params(params)
+        result = self.get_data_by_params(params)
         result_by_years = self.sort_data_by_years(result)
-
         out = {}
 
         for year in result_by_years:
             out[year] = self.calculate_pass_ratio(result_by_years[year])
 
-        return (provinence, out)
+        return (province, out)
 
     def best_pass_ratio(self, year, gender=None):
         params = {
@@ -63,9 +62,8 @@ class Analysis:
 
         all_in_year = self.get_data_by_params(params)
         pass_by_area = self.calculate_pass_ratio(all_in_year)
-        best = sorted(pass_by_area, key=pass_by_area.get, reverse=True)[1]
-        print(pass_by_area)
-        print(sorted(pass_by_area, key=pass_by_area.get, reverse=True))
+        best = sorted(pass_by_area, key=pass_by_area.get, reverse=True)[0]
+
         return (pass_by_area[best], best)
 
     def pass_ratio_regression(self, gender=None):
@@ -75,7 +73,7 @@ class Analysis:
 
         # all areas pass ratio by years dict[year][area] = score
         data_by_years = {}
-        all_ratios    = self.get_data_by_params(params)
+        all_ratios = self.get_data_by_params(params)
         data_by_years = self.sort_data_by_years(all_ratios)
 
         pass_ratio_by_years = {}
@@ -88,17 +86,17 @@ class Analysis:
 
         return regressions
 
-    def compare_pass_ratio(self, provinence_1, provinence_2, gender=None):
-        if provinence_1 is None or provinence_2 is None:
-            raise ValueError("Provinence cannot be empty")
+    def compare_pass_ratio(self, province_1, province_2, gender=None):
+        if province_1 is None or province_2 is None:
+            raise ValueError("province cannot be empty")
         params = {}
-        params[self.area_col] = [provinence_1, provinence_2]
+        params[self.area_col] = province_1.split(',') + province_2.split(',')
 
         if gender is not None:
             params[self.gender_col] = [gender]
 
-        provinence_data = self.get_data_by_params(params)
-        data_by_years   = self.sort_data_by_years(provinence_data)
+        province_data = self.get_data_by_params(params)
+        data_by_years = self.sort_data_by_years(province_data)
         pass_ratio_by_years = {}
         for year in data_by_years:
             pass_ratio_by_years[year] = self.calculate_pass_ratio(data_by_years[
@@ -107,14 +105,14 @@ class Analysis:
         better_in_year = {}
         for year, data in pass_ratio_by_years.items():
             try:
-                p1 = data[provinence_1]
-                p2 = data[provinence_2]
+                p1 = data[province_1]
+                p2 = data[province_2]
                 better_in_year[
-                    year] = provinence_1 if p1 > p2 else provinence_2
+                    year] = province_1 if p1 > p2 else province_2
             except KeyError as e:
                 import warnings
                 warnings.warn(
-                    "There is not enough data for provinence {0} for comparing pass in {1}"
+                    "There is not enough data for province {0} for comparing pass in {1}"
                     .format(e.args[0], year),
                     Warning,
                     stacklevel=4)
@@ -128,7 +126,7 @@ class Analysis:
         result is empty.
         '''
         params = {
-            self.area_col: area,
+            self.area_col: area.split(','),
             self.group_col: 'przystąpiło',
             self.year_col: str(years)
         }
@@ -159,11 +157,11 @@ class Analysis:
 
             people = int(row[self.population_col])
             if row[self.group_col] == 'zdało':
-                updated =  pass_by_area[area]['Pass'] + people
+                updated = pass_by_area[area]['Pass'] + people
                 pass_by_area[area]['Pass'] = updated
 
             if row[self.group_col] == 'przystąpiło':
-                updated =  pass_by_area[area]['All'] + people
+                updated = pass_by_area[area]['All'] + people
                 pass_by_area[area]['All'] = updated
 
         ratio = {}
@@ -208,12 +206,8 @@ Found passed: {1} Found attendance: {2}"""
                         ]
                         years_with_regression.append(Regression(*build))
                 except:
-                    import warnings
-                    warnings.warn(
-                        "Could not fund next year data for area " + area,
-                        Warning,
-                        stacklevel=4
-                    )
+                    self._raise_warning(
+                        "Could not fund next year data for area " + area)
 
         return years_with_regression
 
@@ -233,4 +227,9 @@ Found passed: {1} Found attendance: {2}"""
         return data_by_years
 
     def _raise_warning(self, message):
+        warnings.warn(
+            message,
+            Warning,
+            stacklevel=4
+        )
         pass
